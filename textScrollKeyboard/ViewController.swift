affffb28313669b8a17c05b09960ce522287f2ab
Mugunthan Balakrishnan <mugs@230i.com>
2015-10-13 15:20:36 +0530

    Initial Commit

---
+++ textScrollKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textScrollKeyboard
+//
+//  Created by Mugunthan Balakrishnan on 13/10/15.
+//  Copyright © 2015 230 Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
